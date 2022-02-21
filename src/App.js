e8e3fa9ca73c7a2875499c1b859d93c82e13b2a7
Hemant Kumar Jayas <87421907+hemantjayas@users.noreply.github.com>
2022-02-21 17:08:23 +0530

    a

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import img from "./google.png"
+import './App.css';
+
+function App() {
+
+
+
+
+
+  return (
+    <div className="App">
+      <img src={img} alt="" />
+      <br />
+      <input className='search-box' type="text" />
+    </div>
+  );
+}
+
+export default App;
